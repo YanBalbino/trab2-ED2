ea774b4cc069d9003a92b34f83b4d1b72e01a058
Yan Balbino Nogueira <yanbnogueira@hotmail.com>
2024-09-25 19:38:01 -0300

    fix: mudança na função de hash das duas tabelas

--- utils/TabelaComDuploHash.java
+++ utils/TabelaComDuploHash.java
@@ -1,137 +1,135 @@
 package utils;
 
+
 import components.OrdemServico;
+
 
 public class TabelaComDuploHash{
     int M;
-	No tabela[];
+    No tabela[];
     int primoMenor;
-	
-	public TabelaComDuploHash(int tam) {
-		
-		this.M = tam;
-		this.tabela = new No[this.M];
-	
-	}
+   
+    public TabelaComDuploHash(int tam) {
+       
+        this.M = tam;
+        this.tabela = new No[this.M];
+   
+    }
 
-	public boolean isFull(){
-		for (int i = 0; i < this.M; i++){
-			if (this.tabela[i] == null){
-				return false;
-			}
-		}
-		return true;
-	}
 
-	private int hashA(int ch){
-		//return ch % this.M;
-		 double A = (Math.sqrt(5) - 1) / 2; 
-		return (int) (M * ((ch * A) % 1)); 
-	}
+    public boolean isFull(){
+        for (int i = 0; i < this.M; i++){
+            if (this.tabela[i] == null){
+                return false;
+            }
+        }
+        return true;
+    }
 
-	private int hashB(int ch){
-		return 1 + (ch % (this.M - 1)); // -1 pra garantir um primo menor que M (20 - 1)
-	}
 
-	public int hash(int ch, int k) {
-		return (hashA(ch) + k*(hashB(ch))) % this.M; 
-	}
-	
-	private void reordenar(){
-		No antigaTabela[] = this.tabela;
-		this.tabela = new No[this.M];
-		for (No no : antigaTabela){
-			if (no != null){
-				inserir(no.os);
-			}
-		}
-	};
+    private int hash(int ch){
+         double A = (Math.sqrt(5) - 1) / 2;
+        return (int) (M * ((ch * A) % 1));
+    }
 
-	public void inserir(OrdemServico nova) {
 
-		int tentativa = 0;
-		int h = this.hash(nova.getCodigo(), tentativa);
-		
-		while(this.tabela[h] != null && tentativa < this.M) {
-			
-			if (this.tabela[h].os.getCodigo() == nova.getCodigo()) {
-				break;
-			}	
-			
-			h = this.hash(nova.getCodigo(), ++tentativa);
-			System.out.println("Tentativa: " + tentativa + " com o hash: " + h);
-			
-		}
-			
-		if (this.tabela[h] == null) {
-			
-			this.tabela[h] = new No();
-			this.tabela[h].os = nova;
-			return;
-		}
+    public int tentativaLinear(int ch, int k) {
+        return (hash(ch) + k) % this.M;
+    }
 
-		System.out.println("Quantidade de tentativas excedeu o limite");
-	}
-	
-	public No buscar(int codigo) {
-		int tentativa = 0;
-		int h = this.hash(codigo, tentativa);
-		
-		while(this.tabela[h] != null && tentativa < this.M) {
-			
-			if (this.tabela[h].os.getCodigo() == codigo) {
-				return this.tabela[h];
-			}	
-			
-			h = this.hash(codigo, ++tentativa);
-		}
-		return null; 
-		
-	}
-	
-	public void alterar(int codigo, OrdemServico alterada){
-		
-		int tentativa = 0;
-		int h = this.hash(codigo, tentativa);
-		
-		while(this.tabela[h] != null) {
-			
-			if (this.tabela[h].os.getCodigo() == codigo) {
-				this.tabela[h].os = alterada;
-				break;
-			}	
-			
-			h = this.hash(codigo, ++tentativa);
-		}
-	}
 
-	public void remover(int codigo) {
-		int tentativa = 0;
-		int h = this.hash(codigo, tentativa);
-		
-		while(this.tabela[h] != null && tentativa < this.M) {
-			
-			if (this.tabela[h].os.getCodigo() == codigo) {
-				this.tabela[h] = null;
-				break;
-			}	
-			
-			h = this.hash(codigo, ++tentativa);
-		}
-	}
+    public void inserir(OrdemServico nova) {
 
-	public void imprimirTabelaHash() {
-		
-		for(int i = 0; i < this.M; i++) {
-			
-			if (this.tabela[i] != null)
-				System.out.println(i + " --> " + this.tabela[i].os.getCodigo() + " | " + this.tabela[i].os.getNome() 
-				+ " | " + this.tabela[i].os.getDescricao() + " | " + this.tabela[i].os.getData() + "\n");
-			else
-				System.out.println(i);
-		}
-		
-	}
+
+        int tentativa = 0;
+        int h = this.tentativaLinear(nova.getCodigo(), tentativa);
+       
+        while(this.tabela[h] != null && tentativa < this.M) {
+           
+            if (this.tabela[h].os.getCodigo() == nova.getCodigo()) {
+                break;
+            }  
+           
+            h = this.tentativaLinear(nova.getCodigo(), ++tentativa);
+        }
+           
+        if (this.tabela[h] == null) {
+           
+            this.tabela[h] = new No();
+            this.tabela[h].os = nova;
+            return;
+        }
+
+
+        System.out.println("Quantidade de tentativas excedeu o limite");
+    }
+   
+    public No buscar(int codigo) {
+        int tentativa = 0;
+        int h = this.tentativaLinear(codigo, tentativa);
+       
+        while(this.tabela[h] != null && tentativa < this.M) {
+           
+            if (this.tabela[h].os.getCodigo() == codigo) {
+                return this.tabela[h];
+            }  
+           
+            h = this.tentativaLinear(codigo, ++tentativa);
+        }
+        return null;
+       
+    }
+   
+    public void alterar(int codigo, OrdemServico alterada){
+       
+        int tentativa = 0;
+        int h = this.tentativaLinear(codigo, tentativa);
+       
+        while(this.tabela[h] != null) {
+           
+            if (this.tabela[h].os.getCodigo() == codigo) {
+                this.tabela[h].os = alterada;
+                break;
+            }  
+           
+            h = this.tentativaLinear(codigo, ++tentativa);
+        }
+    }
+
+
+    public void remover(int codigo) {
+        int tentativa = 0;
+        int h = this.tentativaLinear(codigo, tentativa);
+       
+        while(this.tabela[h] != null && tentativa < this.M) {
+           
+            if (this.tabela[h].os.getCodigo() == codigo) {
+                this.tabela[h] = null;
+                break;
+            }  
+           
+            h = this.tentativaLinear(codigo, ++tentativa);
+        }
+    }
+
+
+    public void imprimirTabelaHash() {
+       
+        for(int i = 0; i < this.M; i++) {
+           
+            if (this.tabela[i] != null)
+                System.out.println(i + " --> " + this.tabela[i].os.getCodigo() + " | " + this.tabela[i].os.getNome()
+                + " | " + this.tabela[i].os.getDescricao() + " | " + this.tabela[i].os.getData() + "\n");
+            else
+                System.out.println(i);
+        }
+       
+    }
 }
+
+
+
+
+
 
 
